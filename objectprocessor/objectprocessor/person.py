c4a52fe38cf57c52803ead96c14d03b90ed3944c
Orion Grieco <46628700+Orion-Grieco@users.noreply.github.com>
2024-02-09 09:26:49 -0500

    Update person.py

--- objectprocessor/objectprocessor/person.py
+++ objectprocessor/objectprocessor/person.py
@@ -1,16 +1,35 @@
 """Represent a person using an object-oriented approach."""
 
+import collections
+import itertools
 from typing import List
 
-from objectprocessor import constants
 
-# TODO: define the index locations for the person
-person_index = None
+def create_constants(name, *args, **kwargs):
+    """Create a namedtuple of constants."""
+    # the constants are created such that:
+    # the name is the name of the namedtuple
+    # for *args with "Constant_Name" or **kwargs with Constant_Name = "AnyConstantName"
+    # note that this creates a constant that will
+    # throw an AttributeError when attempting to redefine
+    new_constants = collections.namedtuple(name, itertools.chain(args, kwargs.keys()))
+    return new_constants(*itertools.chain(args, kwargs.values()))
 
-# TODO: define the names of the attributes for the person
-person_attributes = None
 
-# ruff: noqa: PLR0913
+# define the index locations for the person
+person_index = create_constants(
+    "person", Name=0, Country=1, Phone_Number=2, Job=3, Email=4
+)
+
+# define the names of the attributes for the person
+person_attribute = create_constants(
+    "person",
+    Name="name",
+    Country="country",
+    Phone_Number="phone_number",
+    Job="job",
+    Email="email",
+)
 
 
 class Person:
@@ -19,6 +38,7 @@ class Person:
     def __init__(
         self, name: str, country: str, phone_number: str, job: str, email: str
     ) -> None:
+        """Define the constructor for a person."""
         self.name = name
         self.country = country
         self.phone_number = phone_number
@@ -26,13 +46,15 @@ class Person:
         self.email = email
 
     def __repr__(self) -> str:
-        # TODO: return a textual representation of a person
-        # TODO: see the output inside of the README.md file
-        return ""
+        """Return a textual representation of the person."""
+        return f"{self.name} is a {self.job} who lives in {self.country}. You can call this person at {self.phone_number} and email them at {self.email}"
 
     def create_list(self) -> List[str]:
-        # TODO: create a list of strings representing a person
-        # where the locations of the index go in the order
-        # for which the attributes are defined in the constructor
+        """Create a list of strings representing the person."""
         details = []
+        details.append(self.name)
+        details.append(self.country)
+        details.append(self.phone_number)
+        details.append(self.job)
+        details.append(self.email)
         return details
